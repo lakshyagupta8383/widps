bfe0191f432ae94f4a10857cd866152966dc74e0
lakshyagupta8383 <guptalakshya8383@gmail.com>
2025-12-25 22:56:16 +0530

    implementing client_detector which detects new clients, disappeared client and if client switched ap

---
+++ data_layer/events/detectors/client_detectors.py
@@ -0,0 +1,48 @@
+from time import time
+from events.types import Event, EventType
+
+
+def detect_clients(prev, curr):
+    events = []
+
+    for mac, client in curr.clients.items():
+        if mac not in prev.clients: # checking new devices
+            events.append(
+                Event(
+                    type=EventType.NEW_CLIENT,
+                    timestamp=time(),
+                    payload={
+                        "station": mac,
+                        "bssid": client.bssid,
+                        "signal": client.signal,
+                    },
+                )
+            )
+        else:
+            old = prev.clients[mac]
+            if old.bssid != client.bssid: # checking if client switched ap
+                events.append(
+                    Event(
+                        type=EventType.CLIENT_ROAMING,
+                        timestamp=time(),
+                        payload={
+                            "station": mac,
+                            "from_bssid": old.bssid,
+                            "to_bssid": client.bssid,
+                        },
+                    )
+                )
+
+    for mac, client in prev.clients.items():
+        if mac not in curr.clients: #if client disappeared 
+            events.append(
+                Event(
+                    type=EventType.CLIENT_GONE,
+                    timestamp=time(),
+                    payload={
+                        "station": mac,
+                    },
+                )
+            )
+
+    return events
